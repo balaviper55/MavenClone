1d633601f1191dc71ceec56c68dc3d2883b9848a
balaviper55 <balaviper55@gmail.com>
2022-09-20 13:40:55 +0530

    message

---
+++ src/test/java/com/org/Class1.java
@@ -0,0 +1,15 @@
+package com.org;
+
+public class Class1 {
+	private void methode1() {
+		System.out.println("1");
+	}
+	private void methode() {
+		System.out.println("2");
+
+	}
+	private void methode3() {
+		System.out.println("3");
+	}
+
+}
