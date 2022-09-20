03f4a484c8f3419ea9f650cc4b57868e5071603d
balaviper55 <balaviper55@gmail.com>
2022-09-20 13:46:56 +0530

    update codes

--- src/test/java/com/org/Class1.java
+++ src/test/java/com/org/Class1.java
@@ -11,5 +11,15 @@ public class Class1 {
 	private void methode3() {
 		System.out.println("3");
 	}
+	private void methode4() {
+		System.out.println("4");
+	}
+	private void methode5() {
+		System.out.println("5");
+
+	}
+	private void methode6() {
+		System.out.println("6");
+	}
 
 }
